27fc39c0ffcfbe6d9113a020065c87dc987e99af
lopu <ireply@myleisure.com.au>
2018-10-27 19:12:11 +1000

    ready

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const testImport1 = require('./testImport1')
 const testImport2 = require('./testImport2')
+const test = require('./testImport3/test')
 
 let exp = {
 	testImport1,
-	testImport2
+	testImport2,
+	test
 }
 
 module.exports = exp
---
+++ src/testImport3/test.js
@@ -0,0 +1,6 @@
+import { test1 } from './module1'
+
+export function test(test){
+	console.log(test1)
+	console.log(test)
+}
